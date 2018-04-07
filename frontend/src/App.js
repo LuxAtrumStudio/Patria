190a111009eda622d5e98a1f990e197586f52d47
Arden Rasmussen <ardenisthebest@gmail.com>
2018-04-06 19:40:44 -0700

    Began work on frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Clock from './clock.js';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Patria</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div class="mdl-grid">
+        <div className="mdl-layout-spacer"></div>
+        <Clock />
+        <div className="mdl-layout-spacer"></div>
       </div>
     );
   }
