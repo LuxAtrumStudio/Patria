663387c49ce17f97f9ea0b8f36b1461ab8e4df57
Arden Rasmussen <ardenisthebest@gmail.com>
2018-11-07 12:11:46 -0800

    modified token selection

--- client/src/views/Settings.vue
+++ client/src/views/Settings.vue
@@ -60,15 +60,59 @@
               </thead>
               <tbody>
                 <tr :class="{ 'is-selected': selected === index }" v-for="(token, index) in userTokens" v-on:click="selected=index">
-                  <th>{{ token.api }}</th>
-                  <td>{{ token.token }}</td>
-                  <td>{{ token.options }}</td>
+                  <th v-if="selected !== index">{{ token.api }}</th>
+                  <th v-else><input class="input" type="text" v-model="token.api"></th>
+                  <td v-if="selected !== index">{{ token.token }}</td>
+                  <td v-else><input class="input" type="text" v-model="token.token"></td>
+                  <td v-if="selected !== index">{{ token.options }}</td>
+                  <td v-else><input class="input" type="text" v-model="token.options"></td>
+                  <td v-if="selected === index"><button class="button is-danger"><span class="icon"><i class="fas fa-trash" /></span></button></td>
                 </tr>
               </tbody>
             </table>
             <form v-if="selected !== -1">
-              {{ userTokens[selected] }}
+              <div class="columns">
+                <div class="column is-2">
+                  <strong>API</strong>
+                </div>
+                <div class="column is-9">
+                  <input type="text" class="input" placeholder="API" v-model="userTokens[selected].api"/>
+                </div>
+                <div class="column is-1">
+                  <button class="button is-danger" v-on:click="deleteToken()">
+                    <span class="icon">
+                      <i class="fas fa-trash" />
+                    </span>
+                  </button>
+                </div>
+              </div>
+              <div class="columns">
+                <div class="column is-2">
+                  <strong>Token</strong>
+                </div>
+                <div class="column is-9">
+                  <input type="text" class="input" placeholder="Token" v-model="userTokens[selected].token" />
+                </div>
+              </div>
+              <div class="columns">
+                <div class="column is-2">
+                  <strong>Options</strong>
+                </div>
+                <div class="column is-9">
+                  <input type="text" class="input" placeholder="Options" v-model="userTokens[selected].options" />
+                </div>
+              </div>
+              <div class="columns">
+                <div class="column">
+                </div>
+                <div class="column is-2">
+                  <button class="button is-fullwidth is-success">Save</button> 
+                </div>
+                <div class="column is-1">
+                </div>
+              </div>
             </form>
+            <button class="button is-success is-outlined" v-on:click="addToken()">Add Token</button>
           </div>
         </div>
       </div>
@@ -90,5 +134,13 @@ import Login from '@/components/Login.vue';
 export default class Settings extends Vue {
   private userTokens: any = this.$store.state.tokens;
   private selected: number = -1;
+  private addToken() {
+    this.userTokens.push({api: '', token: '', options: ''});
+    this.selected = this.userTokens.length - 1;
+  }
+  private deleteToken() {
+    this.userTokens.splice(this.selected, 1);
+    this.selected = -1;
+  }
 }
 </script>
